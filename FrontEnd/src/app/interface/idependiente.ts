07d54b9b2d8db4049a02e418cdb59a1702c43dea
Leo28Salazar <lsalazar@dos.com.ec>
2025-08-09 09:48:39 -0500

    Cambios, mejoras y actualizaciones  v1.2.5

---
+++ FrontEnd/src/app/interface/idependiente.ts
@@ -0,0 +1,8 @@
+export interface Idependiente {
+    CedulaEmpleado:string; 
+    DepNombre:string; 
+    DepFechaNacimiento:string;  
+    DepDiscapacidad?: boolean; 
+    DepDocumentoUrl:string; 
+    DepRelacion:string; 
+}
